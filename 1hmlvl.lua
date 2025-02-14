e72a8e9dd1f8fa06ea4c46ae429badc80bb9c2e7
tdclmf <104780450+tdclmf@users.noreply.github.com>
2025-02-14 22:31:55 +0300

    Update 1hmlvl.lua

--- 1hmlvl.lua
+++ 1hmlvl.lua
@@ -7,6 +7,14 @@ if game.PlaceId == 17720162456 then
     while not game:GetService("Players").LocalPlayer.PlayerGui.Story.Frame.Visible do
     player.Character.HumanoidRootPart.CFrame = CFrame.new(-341.0579833984375, 16.933761596679688, 3006.642578125)
     humanoid:MoveTo(Vector3.new(-319.4451599121094, 16.70022201538086, 2987.232421875))
+if game:GetService("Players").LocalPlayer.PlayerGui.Story.TeleMenu.Visible then
+    local args = {
+        [1] = workspace.Teleporters.Teleporter2
+    }
+    
+    game:GetService("ReplicatedStorage").Remotes.Teleporters.Leave:FireServer(unpack(args))
+    wait(3)
+        end
     wait(5)
     end
 print('зашел')
