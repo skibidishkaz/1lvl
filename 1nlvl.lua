e45167c33ccee4065b12b6c413d2816781c64fa9
tdclmf <104780450+tdclmf@users.noreply.github.com>
2025-02-16 02:42:07 +0300

    Update 1nlvl.lua

--- 1nlvl.lua
+++ 1nlvl.lua
@@ -5,6 +5,7 @@ local humanoid = character:WaitForChild("Humanoid")
 if game.PlaceId == 17720162456 then
     player.Character.HumanoidRootPart.CFrame = CFrame.new(-341.0579833984375, 16.933761596679688, 3006.642578125)
     humanoid:MoveTo(Vector3.new(-319.4451599121094, 16.70022201538086, 2987.232421875))
+    humanoid.MoveToFinished:Wait()
     wait(5)
     local args = {
         [1] = workspace:WaitForChild("Teleporters"):WaitForChild("Teleporter2"),
