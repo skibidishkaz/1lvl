b8b18b1fb1576d808bedb5c9da478b57c5cbafe1
tdclmf <104780450+tdclmf@users.noreply.github.com>
2025-02-16 11:29:21 +0300

    Update 4hmlvl.lua

--- 4hmlvl.lua
+++ 4hmlvl.lua
@@ -3,19 +3,19 @@ local character = player.Character or player.CharacterAdded:Wait()
 local humanoid = character:WaitForChild("Humanoid")
 
 if game.PlaceId == 17720162456 then
-wait(15)
+    wait(5)
     player.Character.HumanoidRootPart.CFrame = CFrame.new(-341.0579833984375, 16.933761596679688, 3006.642578125)
     humanoid:MoveTo(Vector3.new(-319.4451599121094, 16.70022201538086, 2987.232421875))
+    humanoid.MoveToFinished:Wait()
     wait(5)
-local args = {
-    [1] = workspace.Teleporters.Teleporter1,
-    [2] = 4,
-    [3] = "Hellmode",
-    [4] = true
-}
-
-game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporters"):WaitForChild("ChooseStage"):FireServer(unpack(args))
-
+    local args = {
+        [1] = workspace:WaitForChild("Teleporters"):WaitForChild("Teleporter2"),
+        [2] = 4,
+        [3] = "Hellmode", 
+        [4] = true
+    }
+    game:GetService("ReplicatedStorage"):WaitForChild("Remotes"):WaitForChild("Teleporters"):WaitForChild("ChooseStage"):FireServer(unpack(args))
+    print("выбрал")
 else
     local success, err = pcall(function()
         local realTimer = game:GetService("Workspace").Info.Time.RealTimer
